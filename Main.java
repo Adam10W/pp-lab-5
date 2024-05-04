edc2ac52c3d025c4ab5b4eee374b57d0488c07eb
Adam <wieckowskiadam10@gmail.com>
2024-05-04 10:05:58 +0200

    dodane-zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int num1 = 32;
+        int num2 = 51;
+
+        int sum = MathUtils.add(num1, num2);
+
+        System.out.println("Suma: " + sum);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Wiadomość testowa");
+        emailMessenger.sendMessage("Suma " + num1 + " i " + num2 + " to = " + sum);
     }
 }
